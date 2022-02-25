05441a78091fe039cb30d32d6f61dae9ba45bde4
arjun-dureja <arjun.dureja1000@gmail.com>
2022-02-25 16:07:02 -0500

    optimize python code

--- weather.py
+++ weather.py
@@ -5,224 +5,89 @@ from pprint import pprint
 import time
 import matplotlib.pyplot as plt
 import numpy as np
+
 data_20 = pd.read_csv("2020_data.csv")
 data_21 = pd.read_csv("2021_data.csv")
-# def init_data():
-#     global data_20, data_21
-#     data_20 = pd.read_csv("2020_data.csv")
-#     data_21 = pd.read_csv("2021_data.csv")
-    
 
-# #Predicting 2021 June 15th
+PARAMS = [
+        "Max Temp (°C)",
+        "Min Temp (°C)",
+        "Mean Temp (°C)",
+        "Heat Deg Days (°C)",
+        "Cool Deg Days (°C)",
+        "Total Rain (mm)",
+        "Total Snow (cm)",
+        "Total Precip (mm)",
+        "Snow on Grnd (cm)",
+    ]
+
+NUM_OF_PARAMS = len(PARAMS)
+
+def get_mean(arr):
+    return [arr[i] / 7 for i in range(len(arr))]
+
+# Predicting any day in 2021
 def forecast_day(day):
     if day < 15 or day > 360:
         return None, None
 
     present_days = data_21.loc[day-7:day-1]
-    present_days = present_days[[
-        "Max Temp (°C)",
-        "Min Temp (°C)",
-        "Mean Temp (°C)",
-        "Heat Deg Days (°C)",
-        "Cool Deg Days (°C)",
-        "Total Rain (mm)",
-        "Total Snow (cm)",
-        "Total Precip (mm)",
-        "Snow on Grnd (cm)",
-        # "Dir of Max Gust (10s deg)",
-        # "Spd of Max Gust (km/h)"
-        ]]
+    present_days = present_days[PARAMS]
     present_days = present_days.fillna(0)
     CD = present_days.to_numpy()
     
     prev_days = data_20.loc[day-7:day+6]
-    prev_days = prev_days[[
-    "Max Temp (°C)",
-    "Min Temp (°C)",
-    "Mean Temp (°C)",
-    "Heat Deg Days (°C)",
-    "Cool Deg Days (°C)",
-    "Total Rain (mm)",
-    "Total Snow (cm)",
-    "Total Precip (mm)",
-    "Snow on Grnd (cm)",
-    # "Dir of Max Gust (10s deg)",
-    # "Spd of Max Gust (km/h)"
-    ]]
+    prev_days = prev_days[PARAMS]
     prev_days = prev_days.fillna(0)
     PD = prev_days.to_numpy()
-    # t0 = time.time()
     w_dct = {}
 
-    # pprint(CD)
-    # pprint(PD)
     for index in range(8):
         w_dct[f"W{index+1}"] = PD[index: index+7]
  
     ed_list = []
     # calculating means for present year 7 days
-    present_max = 0
-    present_min = 0
-    present_mean = 0
-    present_head_days = 0
-    present_cool_days = 0
-    present_rain = 0
-    present_snow = 0
-    present_precip = 0
-    present_snow_grnd = 0
+    present_means = [0] * NUM_OF_PARAMS
 
     for line in CD:
-        present_max += line[0] if not math.isnan(line[0]) else 0
-        present_min += line[1] if not math.isnan(line[1]) else 0
-        present_mean += line[2] if not math.isnan(line[2]) else 0
-        present_head_days += line[3] if not math.isnan(line[3]) else 0
-        present_cool_days += line[4] if not math.isnan(line[4]) else 0
-        present_rain += line[5] if not math.isnan(line[5]) else 0
-        present_snow += line[6] if not math.isnan(line[6]) else 0
-        present_precip += line[7] if not math.isnan(line[7]) else 0
-        present_snow_grnd += line[8] if not math.isnan(line[8]) else 0
+        for i in range(len(line)):
+            present_means[i] += line[i] if not math.isnan(line[i]) else 0
 
-
-
-    present_max = present_max / 7
-    present_min = present_min / 7
-    present_mean = present_mean / 7
-    present_head_days = present_head_days / 7
-    present_cool_days = present_cool_days / 7
-    present_rain = present_rain / 7
-    present_snow = present_snow / 7
-    present_precip = present_precip / 7
-    present_snow_grnd = present_snow_grnd / 7
-
-
+    present_means = get_mean(present_means)
 
     # mean of parameters for each window
     for line in w_dct:
-        prev_max = 0
-        prev_min = 0
-        prev_mean = 0
-        prev_head_days = 0
-        prev_cool_days = 0
-        prev_rain = 0
-        prev_snow = 0
-        prev_precip = 0
-        prev_snow_grnd = 0
+        prev_means = [0] * NUM_OF_PARAMS
         for values in w_dct[line]:
-            prev_max += values[0]
-            prev_min += values[1]
-            prev_mean += values[2]
-            prev_head_days += values[3]
-            prev_cool_days += values[4]
-            prev_rain += values[5]
-            prev_snow += values[6]
-            prev_precip += values[7]
-            prev_snow_grnd += values[8]
+            for i in range(len(prev_means)):
+                prev_means += values[i]
 
-        prev_max = prev_max
-        prev_min = prev_min
-        prev_mean = prev_mean
-        prev_head_days = prev_head_days
-        prev_cool_days = prev_cool_days
-        prev_rain = prev_rain
-        prev_snow = prev_snow
-        prev_precip = prev_precip
-        prev_snow_grnd = prev_snow_grnd
+        prev_means = get_mean(prev_means)
 
-        euclid_distance = math.sqrt(
-            (present_max - prev_max)**2 +
-            (present_min - prev_min)**2 +
-            (present_mean - prev_mean)**2 +
-            (present_head_days - prev_head_days)**2 +
-            (present_cool_days - prev_cool_days)**2 +
-            (present_rain - prev_rain)**2 +
-            (present_snow - prev_snow)**2 +
-            (present_precip - prev_precip)**2 +
-            (present_snow_grnd - prev_snow_grnd)**2
-            )
-
+        euclid_distance = sum([(present_means[i] - prev_means[i]) ** 2 for i in range(len(present_means))])        
         ed_list.append(euclid_distance)
+
     min_ed = min(ed_list)
     index_min_ed = indexOf(ed_list, min_ed)
     selected_prev_window = w_dct.get(f"W{index_min_ed+1}")
 
-    sel_max = 0
-    sel_min = 0
-    sel_mean = 0
-    sel_head_days = 0
-    sel_cool_days = 0
-    sel_rain = 0
-    sel_snow = 0
-    sel_precip = 0
-    sel_snow_grnd = 0
+    sel_means = [0] * NUM_OF_PARAMS
 
     for line in selected_prev_window:
-        sel_max += line[0]
-        sel_min += line[1]
-        sel_mean += line[2]
-        sel_head_days += line[3]
-        sel_cool_days += line[4]
-        sel_rain += line[5]
-        sel_snow += line[6]
-        sel_precip += line[7]
-        sel_snow_grnd += line[8]
-        
-    sel_max = sel_max / 7
-    sel_min = sel_min / 7
-    sel_mean = sel_mean / 7
-    sel_head_days = sel_head_days / 7
-    sel_cool_days = sel_cool_days / 7
-    sel_rain = sel_rain / 7
-    sel_snow = sel_snow / 7
-    sel_precip = sel_precip / 7
-    sel_snow_grnd = sel_snow_grnd / 7
+        for i in range(len(line)):
+            sel_means[i] += line[i]
 
-    pred_max = (present_max + sel_max) / 2
-    pred_min = (present_min + sel_min) / 2
-    pred_mean = (present_mean + sel_mean) / 2
-    pred_head_days = (present_head_days + sel_head_days) / 2
-    pred_cool_days = (present_cool_days + sel_cool_days) / 2
-    pred_rain = (present_rain + sel_rain) / 2
-    pred_snow = (present_snow + sel_snow) / 2
-    pred_precip = (present_precip + sel_precip) / 2
-    pred_snow_grnd = (present_snow_grnd + sel_snow_grnd) / 2
+    sel_means = get_mean(sel_means) 
+    
+    pred_arr = [(present_means[i] + sel_means[i]) / 2 for i in range(len(sel_means))]
 
     actual_data = data_21.loc[day]
     # actual_data = data_21[(data_21["Month"] == 6) & (data_21["Day"] == 15)]
     actual_data = actual_data.fillna(0)
-    actual_values = actual_data[[
-        "Max Temp (°C)",
-        "Min Temp (°C)",
-        "Mean Temp (°C)",
-        "Heat Deg Days (°C)",
-        "Cool Deg Days (°C)",
-        "Total Rain (mm)",
-        "Total Snow (cm)",
-        "Total Precip (mm)",
-        "Snow on Grnd (cm)",
-        ]].to_numpy().flatten()
+    actual_values = actual_data[PARAMS].to_numpy().flatten()
 
     # print("Prediction for June 15th, 2021 \n-----------------------------")
 
-    title_arr = ["Max Temp (°C)",
-        "Min Temp (°C)",
-        "Mean Temp (°C)",
-        "Heat Deg Days (°C)",
-        "Cool Deg Days (°C)",
-        "Total Rain (mm)",
-        "Total Snow (cm)",
-        "Total Precip (mm)",
-        "Snow on Grnd (cm)"]
-    pred_arr = [
-        pred_max,
-        pred_min,
-        pred_mean,
-        pred_head_days,
-        pred_cool_days,
-        pred_rain,
-        pred_snow,
-        pred_precip,
-        pred_snow_grnd
-    ]
     # print(" \t\t\t\t Actual | Predicted")
     # for i in range(len(title_arr)):
     #     print(f"{title_arr[i]}           \t|  {actual_values[i]} | {round(pred_arr[i], 2)}")
@@ -262,5 +127,3 @@ if __name__ == "__main__":
     # # pprint(data_20.loc[0,"Total Rain (mm)"])
     
     # # forecast_day(168)
-
-

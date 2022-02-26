31b568a25c11cd0fe50af429354500140db4c11e
arjun-dureja <arjun.dureja1000@gmail.com>
2022-02-26 12:45:59 -0500

    Rewrite step 6 to improve accuracy

--- weather.py
+++ weather.py
@@ -24,7 +24,7 @@ PARAMS = [
 NUM_OF_PARAMS = len(PARAMS)
 
 def get_mean(arr):
-    return [arr[i] / 7 for i in range(len(arr))]
+    return [val / 7 for val in arr]
 
 # Predicting any day in 2021
 def forecast_day(day):
@@ -35,52 +35,71 @@ def forecast_day(day):
     present_days = present_days[PARAMS]
     present_days = present_days.fillna(0)
     CD = present_days.to_numpy()
-    
+
     prev_days = data_20.loc[day-7:day+6]
     prev_days = prev_days[PARAMS]
+
     prev_days = prev_days.fillna(0)
     PD = prev_days.to_numpy()
-    w_dct = {}
 
-    for index in range(8):
-        w_dct[f"W{index+1}"] = PD[index: index+7]
- 
-    ed_list = []
+    windows = []
+    for i in range(len(PD) - 6):
+        windows.append(PD[i: i+7])
+
     # calculating means for present year 7 days
-    present_means = [0] * NUM_OF_PARAMS
-
+    present_sums = [0] * NUM_OF_PARAMS
     for line in CD:
         for i in range(len(line)):
-            if not math.isnan(line[i]): present_means[i] += line[i] 
-
-    present_means = get_mean(present_means)
+            if not math.isnan(line[i]): present_sums[i] += line[i] 
+    present_means = get_mean(present_sums)
 
     # mean of parameters for each window
-    for line in w_dct:
-        prev_means = [0] * NUM_OF_PARAMS
-        for values in w_dct[line]:
-            for i in range(len(prev_means)):
-                prev_means[i] += values[i]
+    ed_list = []
+    for window in windows:
+        prev_sums = [0] * NUM_OF_PARAMS
+        for vals in window:
+            for i in range(len(prev_sums)):
+                prev_sums[i] += vals[i]
+        prev_means = get_mean(prev_sums)
 
-        prev_means = get_mean(prev_means)
-
-        euclid_distance = sum([(present_means[i] - prev_means[i]) ** 2 for i in range(len(present_means))])        
+        euclid_distance = sum([(present_means[i] - prev_means[i]) ** 2 for i in range(len(present_means))])
         ed_list.append(euclid_distance)
 
     min_ed = min(ed_list)
     index_min_ed = indexOf(ed_list, min_ed)
-    selected_prev_window = w_dct.get(f"W{index_min_ed+1}")
+    selected_prev_window = windows[index_min_ed]
 
-    sel_means = [0] * NUM_OF_PARAMS
+    present_variation_vector = []
+    for i in range(len(CD[0])):
+        vector = []
+        for j in range(len(CD)-1):
+            vector.append(CD[j+1][i] - CD[j][i])
+        present_variation_vector.append(vector)
 
-    for line in selected_prev_window:
-        for i in range(len(line)):
-            sel_means[i] += line[i]
-
-    sel_means = get_mean(sel_means) 
+    prev_variation_vector = []
+    for i in range(len(selected_prev_window[0])):
+            vector = []
+            for j in range(len(selected_prev_window)-1):
+                vector.append(selected_prev_window[j+1][i] - selected_prev_window[j][i])
+            prev_variation_vector.append(vector)
     
-    pred_arr = [(present_means[i] + sel_means[i]) / 2 for i in range(len(sel_means))]
+    mean_present_var = []
+    for i in range(len(present_variation_vector)):
+        mean_present_var.append(sum(present_variation_vector[i]) / len(present_variation_vector[i]))
 
+    mean_prev_var = []
+    for i in range(len(prev_variation_vector)):
+        mean_prev_var.append(sum(prev_variation_vector[i]) / len(prev_variation_vector[i]))
+
+    mean_variation_vector = []
+    for i in range(len(mean_present_var)):
+        mean_variation_vector.append((mean_present_var[i] + mean_prev_var[i]) / 2)
+    
+    prev_day = CD[-1]
+    for i in range(len(prev_day)):
+        prev_day[i] += mean_variation_vector[i]
+    pred_arr = prev_day.tolist()
+    
     actual_data = data_21.loc[day]
     actual_data = actual_data.fillna(0)
     actual_values = actual_data[PARAMS].to_numpy().flatten()
@@ -89,17 +108,17 @@ def forecast_day(day):
 
 if __name__ == "__main__":
     t0 = time.time()
-    x = np.arange(0, 365)
+    day_range = np.arange(0, 365)
     actual_list = []
     pred_list = []
-    for val in x:
-        temp_actual, temp_pred = forecast_day(val+1)
-        if not temp_pred == None:
+    for day in day_range:
+        temp_actual, temp_pred = forecast_day(day)
+        if temp_pred:
             actual_list.append(list(temp_actual))
             pred_list.append(temp_pred)
         else:
-            actual_list.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
-            pred_list.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+            actual_list.append([0] * NUM_OF_PARAMS)
+            pred_list.append([0] * NUM_OF_PARAMS)
     print(f"\nTotal time: {time.time() - t0}")
 
     actual_max_lst = []
@@ -108,6 +127,9 @@ if __name__ == "__main__":
         actual_max_lst.append(line[0])
     for line in pred_list:
         pred_max_lst.append(line[0])
-    plt.plot(x, actual_max_lst, label='Actual', color = "red")
-    plt.plot(x, pred_max_lst, label='Prediction', color = "blue")
+
+    plt.plot(day_range, actual_max_lst, label='Actual', color = "red")
+    plt.plot(day_range, pred_max_lst, label='Prediction', color = "blue")
+    plt.title("Max Temp (°C) - Actual vs Prediction")
+    plt.legend(['Actual', 'Prediction'])
     plt.show()
